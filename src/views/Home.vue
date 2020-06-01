4ee48e15b5d19133935a7dbdefff82ea353a181a
Lexa <vivetalexandra@gmail.com>
2020-06-01 20:40:23 +0200

    refactor: code style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :msg="msg"></HelloWorld>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
 </script>
